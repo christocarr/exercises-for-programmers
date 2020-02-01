ef608da238c7e7e0ee0dbebb2404f6f6d9d978df
Chris Carr <christocarr@outlook.com>
2020-02-01 10:19:16 +0000

    Add eventhandlers for inputs

--- exercise three/index.js
+++ exercise three/index.js
@@ -1,2 +1,13 @@
 /*Create a program that asks for a quote and the author */
 
+const quoteInput = document.getElementById('quote')
+const authorInput = document.getElementById('author')
+let quote = '';
+let author = '';
+quoteInput.addEventListener('change', (ev) =>{
+  quote = ev.target.value
+})
+
+authorInput.addEventListener('change', (ev) => {
+  author = ev.target.value
+})
