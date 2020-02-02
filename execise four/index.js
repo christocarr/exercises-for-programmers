bd3df085b6e2f441158f6e87c4ad9f5e63804f29
Chris Carr <christocarr@outlook.com>
2020-02-02 17:20:26 +0000

    Add methods to get input elements

---
+++ execise four/index.js
@@ -0,0 +1,4 @@
+const nounInput = document.getElementById('noun') 
+const verbInput = document.getElementById('verb') 
+const adverbInput = document.getElementById('adverb') 
+const adjectiveInput = document.getElementById('adjective') 
