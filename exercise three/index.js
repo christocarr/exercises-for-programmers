b97507c653b2a2a808b059ee98e3098611d0b632
Chris Carr <christocarr@outlook.com>
2020-02-02 17:06:44 +0000

    Add eventlistners for inputs

--- exercise three/index.js
+++ exercise three/index.js
@@ -1,18 +1,20 @@
 /*Create a program that asks for a quote and the author */
 
-const quoteInput = document.getElementById('quote')
-const authorInput = document.getElementById('author')
-const submitButton = document.getElementById('submit')
+const quoteInput = document.getElementById('quote');
+const authorInput = document.getElementById('author');
+const submitButton = document.getElementById('submit');
+const outputDiv = document.getElementById('output');
 let quote = '';
 let author = '';
-quoteInput.addEventListener('change', (ev) =>{
-  quote = ev.target.value
-})
+quoteInput.addEventListener('change', ev => {
+  quote = ev.target.value;
+});
 
-authorInput.addEventListener('change', (ev) => {
-  author = ev.target.value
-})
+authorInput.addEventListener('change', ev => {
+  author = ev.target.value;
+});
 
 submitButton.addEventListener('click', () => {
-  
-})
+  const output = `${author} says, "${quote}"`;
+  outputDiv.innerHTML = output;
+});
