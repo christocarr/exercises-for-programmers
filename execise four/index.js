3fe6b81de8611270a58398a135e57ff5fb347f2f
Chris Carr <christocarr@outlook.com>
2020-02-02 17:32:43 +0000

    Add eventlistners to input elements

--- execise four/index.js
+++ execise four/index.js
@@ -2,6 +2,24 @@ const nounInput = document.getElementById('noun')
 const verbInput = document.getElementById('verb') 
 const adverbInput = document.getElementById('adverb') 
 const adjectiveInput = document.getElementById('adjective')
-const makeMadLibButton = document.getElementById('makeMadLib').addEventListener('click', () => {
-  console.log('click')
+const makeMadLibButton = document.getElementById('makeMadLib')
+
+let noun; 
+let verb;
+let adverb;
+let adjective;
+nounInput.addEventListener('change', ev => {
+  noun = ev.target.value
+})
+verbInput.addEventListener('change', ev => {
+  verb = ev.target.value
+})
+adverbInput.addEventListener('change', ev => {
+  adverb = ev.target.value
+})
+adjectiveInput.addEventListener('change', ev => {
+  adjective = ev.target.value
+})
+makeMadLibButton.addEventListener('click', () => {
+  console.log(noun)
 })
