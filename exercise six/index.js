12b3d8dd6c867f7d3a8626bb255877cd38da4513
Chris Carr <christocarr@outlook.com>
2020-02-03 19:11:56 +0000

    Add new exercise

---
+++ exercise six/index.js
@@ -0,0 +1,2 @@
+// Create a program that determines how many years you have left until retirement and the year you can retire.
+// It should prompt the user for their current age and the age they want to retire.
