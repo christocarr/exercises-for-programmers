d99d676f3e72f93c26f0d92ab7938698e05c4b87
Chris Carr <christocarr@outlook.com>
2020-02-08 08:09:24 +0000

    Refactor code

--- exercise nine/index.js
+++ exercise nine/index.js
@@ -1,31 +1,27 @@
-const lengthInput = document.getElementById('length')
-const widthInput = document.getElementById('width')
-const outputDiv = document.getElementById('outputDiv')
+const lengthInput = document.getElementById('length');
+const widthInput = document.getElementById('width');
+const outputDiv = document.getElementById('outputDiv');
 
 let length;
 let width;
 
-lengthInput.addEventListener('change', (ev) => {
-  length = parseInt(ev.target.value)
-})
+lengthInput.addEventListener('change', ev => {
+  length = parseInt(ev.target.value);
+});
 
-widthInput.addEventListener('change', (ev) => {
-  width = parseInt(ev.target.value)
-})
+widthInput.addEventListener('change', ev => {
+  width = parseInt(ev.target.value);
+});
 
 function calculate() {
-  const areaToPaint = length * width
-  const oneGallonCovers = 350
-  if (areaToPaint < oneGallonCovers) {
-    let gallonsNeeded = 1
-  } else {
-      gallonsNeeded = Math.ceil(areaToPaint / oneGallonCovers)
-  }
-  createOutput(gallonsNeeded)
+  const areaToPaint = length * width;
+  const oneGallonCovers = 350;
+  let gallonsNeeded = Math.ceil(areaToPaint / oneGallonCovers);
+  createOutput(gallonsNeeded);
 }
 
 function createOutput(gallonsNeeded) {
-  const outputPara = document.createElement('p')
-  outputPara.textContent = `Gallons needed: ${gallonsNeeded}`
-  outputDiv.appendChild(outputPara)
+  const outputPara = document.createElement('p');
+  outputPara.textContent = `Gallons needed: ${gallonsNeeded}`;
+  outputDiv.appendChild(outputPara);
 }
