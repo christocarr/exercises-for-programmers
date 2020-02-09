1f02dfbb9fe9e78727ac03417050053fd26e7ffd
Chris Carr <christocarr@outlook.com>
2020-02-09 17:03:48 +0000

    Add input event listners

--- 11. exercise eleven/index.js
+++ 11. exercise eleven/index.js
@@ -2,3 +2,13 @@ const eurosInput = document.getElementById('eurosInput')
 const exchangeRateInput = document.getElementById('exchangeRateInput')
 const outputDiv = document.getElementById('outputDiv')
 
+let euros
+eurosInput.addEventListener('change', (ev) => {
+  euros = parseInt(ev.target.value)
+})
+
+let exchangeRate
+exchangeRateInput.addEventListener('change', (ev) => {
+  exchangeRate = parseInt(ev.target.value)
+})
+
