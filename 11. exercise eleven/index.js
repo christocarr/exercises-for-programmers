c4929a7a1b723dccc505e19f2046c24a6d98c5f6
Chris Carr <christocarr@outlook.com>
2020-02-09 17:11:49 +0000

    Add conversion to number from event target value

--- 11. exercise eleven/index.js
+++ 11. exercise eleven/index.js
@@ -1,14 +1,14 @@
+
 const eurosInput = document.getElementById('eurosInput')
 const exchangeRateInput = document.getElementById('exchangeRateInput')
 const outputDiv = document.getElementById('outputDiv')
-
 let euros
 eurosInput.addEventListener('change', (ev) => {
-  euros = parseInt(ev.target.value)
+  euros = Number(ev.target.value)
 })
 
 let exchangeRate
 exchangeRateInput.addEventListener('change', (ev) => {
-  exchangeRate = parseInt(ev.target.value)
+  exchangeRate = Number(ev.target.value)
 })
 
