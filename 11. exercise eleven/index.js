46f1e17d7a3d6578acd3bff896942e2bc9213df6
Chris Carr <christocarr@outlook.com>
2020-02-09 17:55:48 +0000

    Clear output when displayoutput is invoked

--- 11. exercise eleven/index.js
+++ 11. exercise eleven/index.js
@@ -1,32 +1,32 @@
+const eurosInput = document.getElementById('eurosInput');
+const exchangeRateInput = document.getElementById('exchangeRateInput');
+const outputDiv = document.getElementById('outputDiv');
+let euros;
+eurosInput.addEventListener('change', ev => {
+  euros = Number(ev.target.value);
+});
 
-const eurosInput = document.getElementById('eurosInput')
-const exchangeRateInput = document.getElementById('exchangeRateInput')
-const outputDiv = document.getElementById('outputDiv')
-let euros
-eurosInput.addEventListener('change', (ev) => {
-  euros = Number(ev.target.value)
-})
-
-let exchangeRate
-exchangeRateInput.addEventListener('change', (ev) => {
-  exchangeRate = Number(ev.target.value)
-})
+let exchangeRate;
+exchangeRateInput.addEventListener('change', ev => {
+  exchangeRate = Number(ev.target.value);
+});
 
 function calculateUSDollars() {
-  const USDollars = (euros * exchangeRate).toFixed(2)
-  displayOutput(euros, exchangeRate, USDollars)
+  const USDollars = (euros * exchangeRate).toFixed(2);
+  displayOutput(euros, exchangeRate, USDollars);
 }
 
 function displayOutput(euros, exchangeRate, USDollars) {
-  const eurosDisplay = document.createElement('p')
-  const exchangeRateDisplay = document.createElement('p')
-  const USDollarsDisplay = document.createElement('p')
+  outputDiv.innerHTML = '';
+  const eurosDisplay = document.createElement('p');
+  const exchangeRateDisplay = document.createElement('p');
+  const USDollarsDisplay = document.createElement('p');
 
-  eurosDisplay.textContent = `Euros: ${euros}`
-  exchangeRateDisplay.textContent = `Exchange rate: ${exchangeRate}`
-  USDollarsDisplay.textContent = `US Dollars: ${USDollars}`
+  eurosDisplay.textContent = `Euros: ${euros}`;
+  exchangeRateDisplay.textContent = `Exchange rate: ${exchangeRate}`;
+  USDollarsDisplay.textContent = `US Dollars: ${USDollars}`;
 
-  outputDiv.appendChild(eurosDisplay)
-  outputDiv.appendChild(exchangeRateDisplay)
-  outputDiv.appendChild(USDollarsDisplay)
+  outputDiv.appendChild(eurosDisplay);
+  outputDiv.appendChild(exchangeRateDisplay);
+  outputDiv.appendChild(USDollarsDisplay);
 }
