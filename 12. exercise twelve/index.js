a7ac726df91ef1c975ffcfbbee213a88ba5ea72a
Chris Carr <christocarr@outlook.com>
2020-02-17 20:30:35 +0000

    Add event listners to inputs

---
+++ 12. exercise twelve/index.js
@@ -0,0 +1,20 @@
+const principalInput = document.getElementById('principalInput')
+const interestInput = document.getElementById('interestInput')
+const yearsInput = document.getElementById('yearsInput')
+
+let principalAmount = 0;
+let interest = 0
+let years = 0
+
+principalInput.addEventListener('change', (ev) => {
+  principalAmount = ev.target.value
+})
+
+interestInput.addEventListener('change', (ev) => {
+  interest = ev.target.value
+})
+
+yearsInput.addEventListener('change', (ev) => {
+  years = ev.target.value
+})
+
