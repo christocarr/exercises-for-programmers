ac18254f335a543d7a28cac9717be8d9727e1e34
Chris Carr <christocarr@outlook.com>
2020-02-21 15:50:20 +0000

    Add event listners to inputs

--- 14. exercise fourteen/index.js
+++ 14. exercise fourteen/index.js
@@ -1,2 +1,11 @@
 const amountInput = document.getElementById('amountInput')
 const stateInput = document.getElementById('stateInput')
+
+let amount
+let state
+amountInput.addEventListener('change', (ev) => {
+  amount = Number(ev.target.value)
+})
+stateInput.addEventListener('change', ev => {
+  state = ev.target.value
+})
