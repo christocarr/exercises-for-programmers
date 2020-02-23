5c21894399379b04adcb14af1c243877a60c53fb
Chris Carr <christocarr@outlook.com>
2020-02-23 15:48:07 +0000

    Add script tag

--- 14. exercise fourteen/index.js
+++ 14. exercise fourteen/index.js
@@ -9,3 +9,21 @@ amountInput.addEventListener('change', (ev) => {
 stateInput.addEventListener('change', ev => {
   state = ev.target.value
 })
+
+function calculateTax() {
+  let tax
+  if (state === 'WI') {
+    tax = 5.5
+  } else {
+    tax = 4.5
+  }
+
+  let taxOnPurchase = amount * (tax / 100)
+  let total = amount + taxOnPurchase
+  displayOutput(taxOnPurchase, total)
+}
+
+function displayOutput(taxOnPurchase, total) {
+  console.log(taxOnPurchase, total)
+}
+
