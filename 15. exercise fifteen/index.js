20f4fb6851df72099b0e53b3215ecc6ee5501913
Chris Carr <christocarr@outlook.com>
2020-02-23 17:14:11 +0000

    Add html inputs

---
+++ 15. exercise fifteen/index.js
@@ -0,0 +1,6 @@
+const usernameInput = document.getElementById('usernameInput')
+const passwordInput = document.getElementById('passwordInput')
+
+function signInValidation() {
+  console.log('click')
+}
