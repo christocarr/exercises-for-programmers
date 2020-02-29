decc58bd24e96ca0372239d1e1285d508bbd3e78
Chris Carr <christocarr@outlook.com>
2020-02-29 12:59:00 +0000

    Add eventlistners

--- 15. exercise fifteen/index.js
+++ 15. exercise fifteen/index.js
@@ -1,7 +1,16 @@
-const usernameInput = document.getElementById('usernameInput')
-const passwordInput = document.getElementById('passwordInput')
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+
+let username, passwordAttempt;
+usernameInput.addEventListener('change', ev => {
+  username = ev.target.value;
+});
+
+passwordInput.addEventListener('change', ev => {
+  passwordAttempt = ev.target.value;
+});
 
 function signInValidation() {
-  const knownPassword = 'Password'
-  
+  const knownPassword = 'Password';
+
 }
