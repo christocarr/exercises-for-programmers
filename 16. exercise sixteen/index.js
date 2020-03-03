df3c4a4dcaa870be4139ff79595ee0d0f0207187
Chris Carr <christocarr@outlook.com>
2020-03-03 18:57:14 +0000

    Add function to display right message

--- 16. exercise sixteen/index.js
+++ 16. exercise sixteen/index.js
@@ -1,1 +1,20 @@
-const ageInput = document.getElementById("ageInput")
+const ageInput = document.getElementById('ageInput');
+
+let age;
+ageInput.addEventListener('change', ev => {
+  age = Number(ev.target.value);
+});
+
+function legalAge() {
+  const legalAge = 18;
+  let message;
+  console.log(age);
+  age >= legalAge
+    ? (message = `You're old enough to drive`)
+    : (message = `You're too young to drive`);
+  displayOutput(message)
+}
+
+function displayOutput(message) {
+  console.log(message)
+}
