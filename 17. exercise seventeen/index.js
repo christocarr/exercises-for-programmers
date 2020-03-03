8efd82d572b0e40237f61800f855c93105db07b9
Chris Carr <christocarr@outlook.com>
2020-03-03 19:31:45 +0000

    Add getElement to get input elements

--- 17. exercise seventeen/index.js
+++ 17. exercise seventeen/index.js
@@ -1,3 +1,8 @@
+const weightInput = document.getElementById("weightInput")
+const AgeInput = document.getElementById("AgeInput")
+const genderInput = document.getElementById("genderInput")
+const lastDrinkTimeInput = document.getElementById("lastDrinkTimeInput")
+
 function calculateBloodAlcohol() {
   console.log('click')
 }
