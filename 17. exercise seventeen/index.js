74abdf7b5c9cc4400bccdc6542242af00196e6ca
Chris Carr <christocarr@outlook.com>
2020-03-06 17:57:46 +0000

    Fix variable name

--- 17. exercise seventeen/index.js
+++ 17. exercise seventeen/index.js
@@ -1,15 +1,15 @@
 const weightInput = document.getElementById('weightInput');
-const ageInput = document.getElementById('AgeInput');
+const numberDinksInput = document.getElementById('drinksInput');
 const genderInput = document.getElementById('genderInput');
 const lastDrinkTimeInput = document.getElementById('lastDrinkTimeInput');
 
-let weight, age, gender, lastDrinkTime;
+let weight, numberOfDrinks, gender, lastDrinkTime;
 weightInput.addEventListener('change', ev => {
   weight = Num(ev.target.value)
 })
 
-ageInput.addEventListener('change', ev => {
-  age = Num(ev.target.value)
+numberDrinksInput.addEventListener('change', ev => {
+  numberDinksInput = Num(ev.target.value)
 })
 
 genderInput.addEventListener('change', ev => {
