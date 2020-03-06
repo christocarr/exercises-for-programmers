4230e3c55ae864fb6c2305d8fc84ccca86b2cc11
Chris Carr <christocarr@outlook.com>
2020-03-06 17:54:34 +0000

    Fix typo

--- 17. exercise seventeen/index.js
+++ 17. exercise seventeen/index.js
@@ -1,10 +1,25 @@
 const weightInput = document.getElementById('weightInput');
-const AgeInput = document.getElementById('AgeInput');
+const ageInput = document.getElementById('AgeInput');
 const genderInput = document.getElementById('genderInput');
 const lastDrinkTimeInput = document.getElementById('lastDrinkTimeInput');
 
 let weight, age, gender, lastDrinkTime;
+weightInput.addEventListener('change', ev => {
+  weight = Num(ev.target.value)
+})
+
+ageInput.addEventListener('change', ev => {
+  age = Num(ev.target.value)
+})
+
+genderInput.addEventListener('change', ev => {
+  gender = Num(ev.target.value)
+})
+
+lastDrinkTimeInput.addEventListener('change', ev => {
+  lastDrinkTime = Num(ev.target.value)
+})
 
 function calculateBloodAlcohol() {
-  console.log('click');
+  console.log(weight, age, gender, lastDrinkTime);
 }
