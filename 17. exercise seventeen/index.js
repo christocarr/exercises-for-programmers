ef454bf51ff40b031691e93cbf6a06978b2662b1
Chris Carr <christocarr@outlook.com>
2020-03-06 17:59:13 +0000

    Fix Number method typo

--- 17. exercise seventeen/index.js
+++ 17. exercise seventeen/index.js
@@ -1,23 +1,23 @@
 const weightInput = document.getElementById('weightInput');
-const numberDinksInput = document.getElementById('drinksInput');
+const numberDrinksInput = document.getElementById('drinksInput');
 const genderInput = document.getElementById('genderInput');
 const lastDrinkTimeInput = document.getElementById('lastDrinkTimeInput');
 
 let weight, numberOfDrinks, gender, lastDrinkTime;
 weightInput.addEventListener('change', ev => {
-  weight = Num(ev.target.value)
+  weight = Number(ev.target.value)
 })
 
 numberDrinksInput.addEventListener('change', ev => {
-  numberDinksInput = Num(ev.target.value)
+  numberDinksInput = Number(ev.target.value)
 })
 
 genderInput.addEventListener('change', ev => {
-  gender = Num(ev.target.value)
+  gender = Number(ev.target.value)
 })
 
 lastDrinkTimeInput.addEventListener('change', ev => {
-  lastDrinkTime = Num(ev.target.value)
+  lastDrinkTime = Number(ev.target.value)
 })
 
 function calculateBloodAlcohol() {
