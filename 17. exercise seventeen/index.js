385c8c6b639aa5925f5e09645f77adc83b1c719a
Chris Carr <christocarr@outlook.com>
2020-03-06 17:50:53 +0000

    Add variables for input values

--- 17. exercise seventeen/index.js
+++ 17. exercise seventeen/index.js
@@ -1,8 +1,10 @@
-const weightInput = document.getElementById("weightInput")
-const AgeInput = document.getElementById("AgeInput")
-const genderInput = document.getElementById("genderInput")
-const lastDrinkTimeInput = document.getElementById("lastDrinkTimeInput")
+const weightInput = document.getElementById('weightInput');
+const AgeInput = document.getElementById('AgeInput');
+const genderInput = document.getElementById('genderInput');
+const lastDrinkTimeInput = document.getElementById('lastDrinkTimeInput');
+
+let weight, age, gender, lastDrinkTime;
 
 function calculateBloodAlcohol() {
-  console.log('click')
+  console.log('click');
 }
