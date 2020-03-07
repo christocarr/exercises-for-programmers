33c8bc1398bb6686b4da9dd0bca9bfee3ace57ef
Chris Carr <christocarr@outlook.com>
2020-03-07 11:18:02 +0000

    Add formula to convert to chosen temp type

--- 18. exercise eighteen/index.js
+++ 18. exercise eighteen/index.js
@@ -1,27 +1,33 @@
-const celsiusOption = document.getElementById('celsiusOption')
-const fahrenheitOption = document.getElementById('fahrenheitOption')
-const tempInput = document.getElementById('tempInput')
+const celsiusOption = document.getElementById('celsiusOption');
+const fahrenheitOption = document.getElementById('fahrenheitOption');
+const tempInput = document.getElementById('tempInput');
 
-let tempType
-let temp
+let tempType;
+let temp;
 celsiusOption.addEventListener('click', ev => {
-  tempType = ev.target.value
-})
+  tempType = ev.target.value;
+});
 fahrenheitOption.addEventListener('click', ev => {
-  tempType = ev.target.value
-})
+  tempType = ev.target.value;
+});
 tempInput.addEventListener('change', ev => {
-  temp = ev.target.value
-})
+  temp = ev.target.value;
+});
 
 function convertTemp() {
-
-  let tempErrorInputMessage, tempTypeErrorMessage
+  let tempErrorInputMessage, tempTypeErrorMessage, convertedTemp;
   if (temp !== Number) {
-    tempErrorInputMessage = 'Temperature must be a number.'
+    tempErrorInputMessage = 'Temperature must be a number.';
   }
   if (!celsiusOption.checked && !fahrenheitOption.checked) {
-    tempTypeErrorMessage = 'Please select a temperature type to convert to.'
+    tempTypeErrorMessage = 'Please select a temperature type to convert to.';
   }
 
+  if (tempType === 'celsius') {
+    convertedTemp = (temp - 32) * 5 / 9
+    console.log(convertedTemp)
+  } else if (tempType === 'fahrenheit') {
+    convertedTemp = (temp *  9 / 5) + 32
+    console.log(convertedTemp)
+  }
 }
