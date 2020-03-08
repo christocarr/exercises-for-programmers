c653b27100ee756cfcc2392d9a48f86017b0629f
Chris Carr <christocarr@outlook.com>
2020-03-08 19:08:28 +0000

    Add eventlistners on input elements

--- 19. exercise nineteen/index.js
+++ 19. exercise nineteen/index.js
@@ -1,4 +1,13 @@
-const weightInput = document.getElementById('weightInput')
-const heightInput = document.getElementById('heightInput')
-const outputDiv = document.getElementById('outputDiv')
+const weightInput = document.getElementById('weightInput');
+const heightInput = document.getElementById('heightInput');
+const outputDiv = document.getElementById('outputDiv');
+
+let weight, height;
+weightInput.addEventListener('change', ev => {
+  weight = Number(ev.target.value)
+})
+heightInput.addEventListener('change', ev => {
+  height = Number(ev.target.value)
+})
+
 
