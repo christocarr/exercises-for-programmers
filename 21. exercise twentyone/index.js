181814706c031bcf0bc2e000c7c03196de36ff3c
Chris Carr <christocarr@outlook.com>
2020-03-09 16:20:27 +0000

    Add eventlistner to month input

--- 21. exercise twentyone/index.js
+++ 21. exercise twentyone/index.js
@@ -1,2 +1,8 @@
 const monthNumInput = document.getElementById('monthNumInput')
 const outputDiv = document.getElementById('outputDiv')
+
+let monthNum
+monthNumInput.addEventListener('change', ev => {
+  monthNum = Number(ev.target.value)
+})
+
