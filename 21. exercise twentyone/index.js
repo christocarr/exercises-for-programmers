85f01b1a8f8b916ef86816e7fd728a64d93aa954
Chris Carr <christocarr@outlook.com>
2020-03-09 16:35:51 +0000

    Add swith statement

--- 21. exercise twentyone/index.js
+++ 21. exercise twentyone/index.js
@@ -6,3 +6,48 @@ monthNumInput.addEventListener('change', ev => {
   monthNum = Number(ev.target.value)
 })
 
+const getMonthName = () => {
+  let monthName
+  switch (monthNum) {
+    case 1:
+      monthName = 'January'
+      break
+    case 2:
+      monthName = 'Febuary'
+      break
+    case 3:
+      monthName = 'March'
+      break
+    case 4:
+      monthName = 'April'
+      break
+    case 5:
+      monthName = 'May'
+      break
+    case 6:
+      monthName = 'June'
+      break
+    case 7:
+      monthName = 'July'
+      break
+    case 8:
+      monthName = 'August'
+      break
+    case 9: 
+      monthName = 'September'
+      break
+    case 10: 
+      monthName = 'October'
+      break
+    case 11: 
+      monthName = 'November'
+      break
+    case 12: 
+      monthName = 'December'
+      break
+    default:
+      const error = `Please enter 1 - 12`
+      break
+  }
+  console.log(monthName)
+}
