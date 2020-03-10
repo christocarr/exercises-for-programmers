1cf75e95b80d08631a4d124318b2811d0306519d
Chris Carr <christocarr@outlook.com>
2020-03-10 13:38:31 +0000

    Add get DOM input element

---
+++ 22. exercise twentytwo/index.js
@@ -0,0 +1,4 @@
+const firstNumberInput = document.getElementById('firstNumberInput')
+const secondNumberInput = document.getElementById('secondNumberInput')
+const thirdNumberInput = document.getElementById('thirdNumberInput')
+const outputDiv = document.getElementById('outputDiv')
