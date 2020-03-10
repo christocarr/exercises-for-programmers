cb3031952879ba0f6cb2fe12055bfe9f4b653ef7
Chris Carr <christocarr@outlook.com>
2020-03-10 14:12:54 +0000

    Add push to add inputs to array

--- 22. exercise twentytwo/index.js
+++ 22. exercise twentytwo/index.js
@@ -3,16 +3,16 @@ const secondNumberInput = document.getElementById('secondNumberInput')
 const thirdNumberInput = document.getElementById('thirdNumberInput')
 const outputDiv = document.getElementById('outputDiv')
 
-let firstNumber, secondNumber, thirdNumber
+let numbersArr = []
 firstNumberInput.addEventListener('change', ev => {
-  firstNumber = Number(ev.target.value)
+  numbersArr.push(Number(ev.target.value))
 })
 secondNumberInput.addEventListener('change', ev => {
-  secondNumber = Number(ev.target.value)
+  numbersArr.push(Number(ev.target.value))
 })
 thirdNumberInput.addEventListener('change', ev => {
-  thirdNumber = Number(ev.target.value)
+  numbersArr.push(Number(ev.target.value))
 })
 const compareNumbers = () => {
-  console.log(firstNumber, secondNumber, thirdNumber)
+  console.log(numbersArr)
 }
