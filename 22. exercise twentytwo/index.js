a02a8b7457ff6a1e54a29f3ae9f64dd0f89e0fd2
Chris Carr <christocarr@outlook.com>
2020-03-10 13:57:46 +0000

    Add eventlistners to inputs

--- 22. exercise twentytwo/index.js
+++ 22. exercise twentytwo/index.js
@@ -2,3 +2,17 @@ const firstNumberInput = document.getElementById('firstNumberInput')
 const secondNumberInput = document.getElementById('secondNumberInput')
 const thirdNumberInput = document.getElementById('thirdNumberInput')
 const outputDiv = document.getElementById('outputDiv')
+
+let firstNumber, secondNumber, thirdNumber
+firstNumberInput.addEventListener('change', ev => {
+  firstNumber = Number(ev.target.value)
+})
+secondNumberInput.addEventListener('change', ev => {
+  secondNumber = Number(ev.target.value)
+})
+thirdNumberInput.addEventListener('change', ev => {
+  thirdNumber = Number(ev.target.value)
+})
+const compareNumbers = () => {
+  console.log(firstNumber, secondNumber, thirdNumber)
+}
