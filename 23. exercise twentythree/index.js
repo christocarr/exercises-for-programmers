bdbe54f798b75c37b56e06b8cd2aaa4ba26ac569
Chris Carr <christocarr@outlook.com>
2020-03-12 14:31:34 +0000

    Add radio buttons and labels

--- 23. exercise twentythree/index.js
+++ 23. exercise twentythree/index.js
@@ -3,11 +3,30 @@ const questionDiv = document.getElementById('questionDiv')
 const start = () => {
   const startButton = this.document.body.firstElementChild
   startButton.classList.add('hide')
-  const startingQues = document.createElement('p')
-  startingQues.textContent = 'Is the car silent when you turn the key?'
-  questionDiv.appendChild(startingQues)
+  newQuestion()
 }
 
+const newQuestion = () => {
+  const startingQues = document.createElement('p')
+  const yesNoDiv = document.createElement('div')
+  const yesInput = document.createElement('input')
+  const noInput = document.createElement('input')
+  const yesLabel = document.createElement('label')
+  const noLabel = document.createElement('label')
+  yesInput.setAttribute('type', 'radio')
+  yesInput.setAttribute('name', 'answer')
+  noInput.setAttribute('type', 'radio')
+  noInput.setAttribute('name', 'answer')
+  yesLabel.textContent = 'Yes'
+  noLabel.textContent = 'No'
+  yesNoDiv.append(yesInput)
+  yesNoDiv.append(yesLabel)
+  yesNoDiv.append(noInput)
+  yesNoDiv.append(noLabel)
+  startingQues.textContent = 'Is the car silent when you turn the key?'
+  questionDiv.append(startingQues)
+  questionDiv.append(yesNoDiv)
+}
 
 // let question;
 // nextButton.addEventListener('click', () => {
