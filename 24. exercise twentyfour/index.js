7edddb477d1fb28be51b488a65f8cddefcc8ce84
Chris Carr <christocarr@outlook.com>
2020-03-13 11:40:33 +0000

    Add event listners to inputs

--- 24. exercise twentyfour/index.js
+++ 24. exercise twentyfour/index.js
@@ -1,3 +1,10 @@
 const firstWordInput = document.getElementById('firstWordInput')
 const secondWordInput = document.getElementById('secondWordInput')
 
+let firstWord, secondWord
+firstWordInput.addEventListener('change', ev => {
+  firstWord = ev.target.value
+})
+secondWordInput.addEventListener('change', ev => {
+  secondWord = ev.target.value
+})
