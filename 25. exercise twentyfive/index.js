1b0687959d3673bc45c94c7c77613fdc58edc570
Chris Carr <christocarr@outlook.com>
2020-03-15 13:08:32 +0000

    Add regex condition to check if string has special char

--- 25. exercise twentyfive/index.js
+++ 25. exercise twentyfive/index.js
@@ -6,7 +6,11 @@ passwordInput.addEventListener('keyup', ev => {
 });
 
 const passwordValidator = passwordString => {
-  let string = passwordString.toLowerCase()
-  const isOnlyChars = /^[a-z]+$/.test(string)
-  const isTooShort = string.length < 8 ? true : false
+  let message = '';
+  let string = passwordString.toLowerCase();
+  const isOnlyLetters = /^[a-z]+$/.test(string);
+  const isOnlyNumbers = /^[0-9]+$/.test(string);
+  const isTooShort = string.length < 8 ? true : false;
+  const hasSpecChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(string);
+  
 };
