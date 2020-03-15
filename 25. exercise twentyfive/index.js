4dc4c8bd7361d0c8a5de57a69563bab83f08bea2
Chris Carr <christocarr@outlook.com>
2020-03-15 13:09:27 +0000

    Add conditions to modify message depending on condition

--- 25. exercise twentyfive/index.js
+++ 25. exercise twentyfive/index.js
@@ -12,5 +12,15 @@ const passwordValidator = passwordString => {
   const isOnlyNumbers = /^[0-9]+$/.test(string);
   const isTooShort = string.length < 8 ? true : false;
   const hasSpecChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(string);
-  
+
+
+  if (isTooShort && isOnlyNumbers) {
+    message = `Your password is very weak please enter more than eight characters and at least one letter`;
+  } else if (isTooShort && isOnlyLetters) {
+    message = `Your password is weak please enter at least one number and more than eight characters`;
+  } else if (!isTooShort && !isOnlyLetters && hasSpecChar) {
+    message = 'Your password is strong';
+  } else {
+    message = 'Your password is very strong';
+  }
 };
