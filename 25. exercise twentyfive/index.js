2a2308c4a4fc76cec2dd40e8b9bf2017e2530f59
Chris Carr <christocarr@outlook.com>
2020-03-16 11:02:43 +0000

    Add to classlist for bad password

--- 25. exercise twentyfive/index.js
+++ 25. exercise twentyfive/index.js
@@ -17,13 +17,20 @@ const passwordValidator = passwordString => {
 
   if (isTooShort && isOnlyNumbers) {
     message = `Your password is very weak please enter more than eight characters and at least one letter`;
+    passwordInput.classList.add('bad-passord')
+    passwordInput.classList.remove('good-password')
   } else if (isTooShort && isOnlyLetters) {
     message = `Your password is weak please enter at least one number and more than eight characters`;
+    passwordInput.classList.add('bad-passord')
+    passwordInput.classList.remove('good-password')
   } else if (!isTooShort && !isOnlyLetters && hasSpecChar) {
     message = 'Your password is strong';
+    passwordInput.classList.remove('bad-passord')
+    passwordInput.classList.add('good')
   } else {
     message = 'Your password is very strong';
     passwordInput.classList.add('good-password')
+    passwordInput.classList.remove('bad-passord')
   }
 
   displayMessage(message)
