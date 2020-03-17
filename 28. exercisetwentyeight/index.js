817b2b4cf4fbc44a1c74529a95267e08f25a4e98
Chris Carr <christocarr@outlook.com>
2020-03-17 16:18:47 +0000

    Remove redundant code

--- 28. exercisetwentyeight/index.js
+++ 28. exercisetwentyeight/index.js
@@ -4,19 +4,6 @@
   const outputDiv = document.getElementById('outputDiv')
   const calcButton = document.getElementById('calcButton')
 
-  // let requiredNumbers = 1
-  // while (requiredNumbers < 6) {
-  //   const div = document.createElement('div')
-  //   const label = document.createElement('label')
-  //   label.textContent = 'Enter a number: '
-  //   const input = document.createElement('input')
-  //   input.setAttribute('type', 'number')
-  //   label.append(input)
-  //   div.append(label)
-  //   inputDiv.append(div)
-  //   requiredNumbers ++
-  // }
-
   const createInput = () => {
     const div = document.createElement('div')
     const label = document.createElement('label')
