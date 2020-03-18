4addf289469f920bcaa774952edf8cc3af7c07a1
Chris Carr <christocarr@outlook.com>
2020-03-18 14:12:28 +0000

    Add eventlistner to input

--- 29. exercise twentynine/index.js
+++ 29. exercise twentynine/index.js
@@ -1,2 +1,20 @@
 const rateInput = document.querySelector('input')
 const outputPara = document.querySelector('p')
+
+const string = /[A-Za-z]*$/
+const startWithZero = /^[1-9][0-9]*$/
+let rate
+rateInput.addEventListener('change', (ev) => {
+  rate =  Number(ev.target.value)
+})
+
+const calculate = () => { 
+  
+  console.log(rate)
+  if (NaN) {
+    outputPara.textContent = `${rate} is an invalid input.`
+  } else {
+    outputPara.textContent = `${72/rate}`
+  }
+  
+}
