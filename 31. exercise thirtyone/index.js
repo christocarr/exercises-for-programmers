a665ad78e197980d0789105bcdeec8c2ae9ebd52
Chris Carr <christocarr@outlook.com>
2020-03-19 16:17:48 +0000

    Add function to calculate heart rate

--- 31. exercise thirtyone/index.js
+++ 31. exercise thirtyone/index.js
@@ -1,11 +1,17 @@
 const ageInput = document.getElementById('ageInput')
 const restingPulseInput = document.getElementById('restingPulseInput')
 
-let age
-let restingPulse
-ageInput.addEventListener('change', (ev) => {
+let age = 0;
+let restingPulse = 0;
+ageInput.addEventListener('keyup', (ev) => {
   age = Number(ev.target.value)
 })
-restingPulseInput.addEventListener('change', (ev) => {
+restingPulseInput.addEventListener('keyup', (ev) => {
   restingPulse = Number(ev.target.value)
 })
+
+const calculate = () => {
+  let intensity = 55
+  const targetHeartRate = (((220 - age) - restingPulse) * intensity) + restingPulse
+  console.log(targetHeartRate)
+}
