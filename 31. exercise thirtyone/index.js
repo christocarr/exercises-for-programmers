ba1fb4ec44cd315d0dde56901f7208fc0ea7ac48
Chris Carr <christocarr@outlook.com>
2020-03-20 13:33:19 +0000

    Add div for output

--- 31. exercise thirtyone/index.js
+++ 31. exercise thirtyone/index.js
@@ -1,17 +1,18 @@
-const ageInput = document.getElementById('ageInput')
-const restingPulseInput = document.getElementById('restingPulseInput')
+const ageInput = document.getElementById('ageInput');
+const restingPulseInput = document.getElementById('restingPulseInput');
+const outputDiv = document.getElementById('outputDiv');
 
 let age = 0;
 let restingPulse = 0;
-ageInput.addEventListener('keyup', (ev) => {
-  age = Number(ev.target.value)
-})
-restingPulseInput.addEventListener('keyup', (ev) => {
-  restingPulse = Number(ev.target.value)
-})
+ageInput.addEventListener('keyup', ev => {
+  age = Number(ev.target.value);
+});
+restingPulseInput.addEventListener('keyup', ev => {
+  restingPulse = Number(ev.target.value);
+});
 
 const calculate = () => {
-  let intensity = 55
-  const targetHeartRate = (((220 - age) - restingPulse) * intensity) + restingPulse
-  console.log(targetHeartRate)
-}
+  let intensity = 0.55; //55%
+  const targetHeartRate = (220 - age - restingPulse) * intensity + restingPulse;
+  console.log(targetHeartRate);
+};
