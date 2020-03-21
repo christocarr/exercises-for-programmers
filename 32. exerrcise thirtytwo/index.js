9b2f64e67a97606813d417603188e330258fae3f
christocarr <christocarr@outlook.com>
2020-03-21 20:34:06 +0000

    Add output div

--- 32. exerrcise thirtytwo/index.js
+++ 32. exerrcise thirtytwo/index.js
@@ -2,11 +2,13 @@
 
 const inputs = document.querySelectorAll('input[type=radio]');
 const startButton = document.querySelector('button');
+const outputDiv = document.querySelector('#outputDiv')
 
 const startGame = () => {
   startButton.classList.add('hide')
-    const selectedInput = [...inputs].filter(input => {
-      return input.checked === true
-    })
-    const selectedInputValue = selectedInput[0].value
-  };
+  const selectedInput = [...inputs].filter(input => {
+    return input.checked === true
+  })
+  const selectedInputValue = selectedInput[0].value
+  
+};
