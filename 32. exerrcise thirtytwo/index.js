585c4e4ee4c0b0b2fcbf841bddc90578f472df54
christocarr <christocarr@outlook.com>
2020-03-21 20:47:41 +0000

    Add function to generate random numbers

--- 32. exerrcise thirtytwo/index.js
+++ 32. exerrcise thirtytwo/index.js
@@ -10,5 +10,16 @@ const startGame = () => {
     return input.checked === true
   })
   const selectedInputValue = selectedInput[0].value
-  
+  generateRandomNumber(selectedInputValue)
 };
+
+const generateRandomNumber = (value) => {
+  let numberToGuess = 0
+  if (value === 'one') {
+    numberToGuess = Math.floor(Math.random() * 10)
+  } else if (value === 'two') {
+    numberToGuess = Math.floor(Math.random() * 100)
+  } else if (value === 'three') {
+    numberToGuess = Math.floor(Math.random() * 1000)
+  }
+}
