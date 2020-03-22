8f718ed67d519144b544072bfee3efede640c7d3
Chris Carr <christocarr@outlook.com>
2020-03-22 14:53:10 +0000

    Add function to create input for guesses

--- 32. exerrcise thirtytwo/index.js
+++ 32. exerrcise thirtytwo/index.js
@@ -2,24 +2,33 @@
 
 const inputs = document.querySelectorAll('input[type=radio]');
 const startButton = document.querySelector('button');
-const outputDiv = document.querySelector('#outputDiv')
+const outputDiv = document.querySelector('#outputDiv');
+
+let numberToGuess = 0;
+let guessInput
 
 const startGame = () => {
-  startButton.classList.add('hide')
+  startButton.classList.add('hide');
   const selectedInput = [...inputs].filter(input => {
-    return input.checked === true
-  })
-  const selectedInputValue = selectedInput[0].value
-  generateRandomNumber(selectedInputValue)
+    return input.checked === true;
+  });
+  const selectedInputValue = selectedInput[0].value;
+  generateRandomNumber(selectedInputValue);
+  outputDiv.append('I have my number. Whats your guess?')
+  guessInputGenerator()
 };
 
-const generateRandomNumber = (value) => {
-  let numberToGuess = 0
+const generateRandomNumber = value => {
   if (value === 'one') {
-    numberToGuess = Math.floor(Math.random() * 10)
+    numberToGuess = Math.floor(Math.random() * 10);
   } else if (value === 'two') {
-    numberToGuess = Math.floor(Math.random() * 100)
+    numberToGuess = Math.floor(Math.random() * 100);
   } else if (value === 'three') {
-    numberToGuess = Math.floor(Math.random() * 1000)
+    numberToGuess = Math.floor(Math.random() * 1000);
   }
+};
+
+const guessInputGenerator = () => {
+  guessInput = document.createElement('input')
+  outputDiv.append(guessInput)
 }
