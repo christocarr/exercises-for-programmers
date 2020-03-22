85550ca7e9b953c866f3830be2b8cb310441900a
Chris Carr <christocarr@outlook.com>
2020-03-22 15:23:06 +0000

    Add function to get random string from ANSWERS

--- 33. exercise thirtythree/index.js
+++ 33. exercise thirtythree/index.js
@@ -1,2 +1,7 @@
 const answers = ['Yes', 'No', 'Maybe', 'Ask again later']
+const outputDiv = document.querySelector('div')
 
+const getAnswer = () => {
+  const randomNumber = Math.floor(Math.random() * 4)
+  outputDiv.textContent = `${answers[randomNumber]}`
+}
