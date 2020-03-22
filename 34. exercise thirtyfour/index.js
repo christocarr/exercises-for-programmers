26cc14adbe428ff1f584eee81b28cb66a47b7c8a
Chris Carr <christocarr@outlook.com>
2020-03-22 15:56:12 +0000

    Add event listner to input

--- 34. exercise thirtyfour/index.js
+++ 34. exercise thirtyfour/index.js
@@ -1,6 +1,9 @@
 
   const EMPLOYEES = ['John Smith', 'Adam Apple', 'Tom Tomms', 'Julia Jewels', 'Carol Cousins']
   const outputDiv = document.getElementById('outputDiv')
+  const nameInput = document.querySelector('input')
+
+  let nameToRemove
 
   const employeeNumbers = `There are ${EMPLOYEES.length} employees:`
   outputDiv.append(employeeNumbers)
@@ -11,3 +14,7 @@
     outputDiv.append(employeeLine)
     iterator ++
   }
+
+  nameInput.addEventListener('change', (ev) => {
+    nameToRemove = ev.target.value
+  })
