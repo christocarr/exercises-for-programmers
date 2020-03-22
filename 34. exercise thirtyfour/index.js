4992e5e74b9554fb4d7a98a8201e469cec76b6ba
Chris Carr <christocarr@outlook.com>
2020-03-22 15:52:41 +0000

    Remove IIFE

--- 34. exercise thirtyfour/index.js
+++ 34. exercise thirtyfour/index.js
@@ -1,2 +1,13 @@
-const EMPLOYEES = ['John Smith', 'Adam Apple', 'Tom Tomms', 'Julia Jewels', 'Carol Cousins']
 
+  const EMPLOYEES = ['John Smith', 'Adam Apple', 'Tom Tomms', 'Julia Jewels', 'Carol Cousins']
+  const outputDiv = document.getElementById('outputDiv')
+
+  const employeeNumbers = `There are ${EMPLOYEES.length} employees:`
+  outputDiv.append(employeeNumbers)
+  let iterator = 0
+  while (iterator < EMPLOYEES.length) {
+    const employeeLine = document.createElement('p')
+    employeeLine.textContent = EMPLOYEES[iterator]
+    outputDiv.append(employeeLine)
+    iterator ++
+  }
