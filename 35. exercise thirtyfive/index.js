d9d27bd5891753b157b07472516d1556fc52e3ac
Chris Carr <christocarr@outlook.com>
2020-03-23 11:39:37 +0000

    Add event listner on input to add names to div

--- 35. exercise thirtyfive/index.js
+++ 35. exercise thirtyfive/index.js
@@ -1,2 +1,15 @@
-const form = document.querySelector('form')
+const outputDiv = document.querySelector('div')
+const nameInput = document.querySelector('input')
+
+let name = ''
+
+nameInput.addEventListener('keyup', (ev) => {
+  name = ev.target.value
+  if (ev.keyCode === 13 ) {
+    const para = document.createElement('p')
+    para.textContent = name
+    outputDiv.append(para)
+    ev.target.value = ''
+  }
+})
 
