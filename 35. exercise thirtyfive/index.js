ef7b6627154a14454ea614438abe636a0c7e3aca
Chris Carr <christocarr@outlook.com>
2020-03-23 12:19:34 +0000

    Add function to select and display a winner

--- 35. exercise thirtyfive/index.js
+++ 35. exercise thirtyfive/index.js
@@ -1,5 +1,6 @@
 const outputDiv = document.querySelector('div')
 const nameInput = document.querySelector('input')
+const winnerDiv = document.getElementById('winnerDiv')
 
 let name = ''
 let nameArr = []
@@ -15,3 +16,9 @@ nameInput.addEventListener('keyup', (ev) => {
   }
 })
 
+const selectWinner = () => {
+  const lengthOfArr = nameArr.length
+  const randomNumber = Math.floor(Math.random() * lengthOfArr)
+  const winner = nameArr.slice(randomNumber, randomNumber + 1)
+  winnerDiv.textContent = `The winner is: ${winner[0]}`
+}
