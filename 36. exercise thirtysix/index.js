9e711662f51df31d6eca73fc764201ceb90447e6
Chris Carr <christocarr@outlook.com>
2020-03-24 15:04:17 +0000

    Add function to calculate average

--- 36. exercise thirtysix/index.js
+++ 36. exercise thirtysix/index.js
@@ -12,10 +12,35 @@ numberInput.addEventListener('keyup', ev => {
     para.textContent = number
     outputDiv.append(para)
     ev.target.value = ''
-    numberArr.push(number)
+    numberArr.push(Number(number))
   }
 });
 
 const calculate = () => {
-  console.log('calc')
+  const sum = numberArr.reduce((acc, num) => {
+    return acc + num 
+  })
+  const average = sum / numberArr.length 
+  console.log(average)
+  const averagePara = document.createElement('p')
+  averagePara.textContent = `The average is ${average}.`
+  calculationOutput.append(averagePara)
+
+  //sort arr lowest to highest
+  const sortedNumArr = numberArr.sort((a,b) => {
+    if (a > b) {
+      return 1
+    }
+    if (a < b) {
+      return -1
+    }
+    return 0
+  })
+  const lowestPara = document.createElement('p')
+  lowestPara.textContent = `The minimum is ${sortedNumArr[0]}.`
+  calculationOutput.append(lowestPara)
+
+  const highestPara = document.createElement('p')
+  highestPara.textContent = `The maximum is ${sortedNumArr[sortedNumArr.length - 1]}.`
+  calculationOutput.append(highestPara)
 }
