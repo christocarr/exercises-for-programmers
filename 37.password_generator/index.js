40512ecf351d528c09bd2c0d511bfb9e3cc88745
Chris Carr <christocarr@outlook.com>
2020-03-25 16:15:10 +0000

    Add inputs for password length, numbers amount and special chars amount

---
+++ 37.password_generator/index.js
@@ -0,0 +1,3 @@
+const lengthInput = document.getElementById('lengthInput')
+const specialCharsInput = document.getElementById('specialCharsInput')
+const numbersInput = document.getElementById('numbersInput')
