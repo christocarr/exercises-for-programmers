4de2917e697d9b8f4cae9e7b4109471ceb98b140
Chris Carr <christocarr@outlook.com>
2020-03-25 16:23:16 +0000

    Add event listners to inputs

--- 37.password_generator/index.js
+++ 37.password_generator/index.js
@@ -1,3 +1,15 @@
 const lengthInput = document.getElementById('lengthInput')
 const specialCharsInput = document.getElementById('specialCharsInput')
 const numbersInput = document.getElementById('numbersInput')
+
+let passwordLength, specialCharsLength, numbersLength 
+
+lengthInput.addEventListener('change', ev => {
+  passwordLength = ev.target.value
+})
+specialCharsInput.addEventListener('change', ev => {
+  specialCharsLength = ev.target.value
+})
+numbersInput.addEventListener('change', ev => {
+  numbersLength = ev.target.value
+})
