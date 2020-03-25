207a2874ccde3d30c0921e4b0590d046ad3970f0
Chris Carr <christocarr@outlook.com>
2020-03-25 17:02:17 +0000

    Add input validation

--- 37.password_generator/index.js
+++ 37.password_generator/index.js
@@ -6,12 +6,37 @@ const outputDiv = document.getElementById('outputDiv')
 let passwordLength, specialCharsLength, numbersLength 
 
 lengthInput.addEventListener('change', ev => {
-  passwordLength = ev.target.value
+  let passwordLengthErrPara = document.createElement('p')
+  if (ev.target.value < 8) {
+    passwordLengthErrPara = document.createElement('p')
+    passwordLengthErrPara.textContent = 'Password must have 8 or more characters.'
+    outputDiv.append(passwordLengthErrPara)
+  } else {
+    passwordLengthErrPara.remove()
+    passwordLength = ev.target.value
+  }
 })
 specialCharsInput.addEventListener('change', ev => {
-  specialCharsLength = ev.target.value
+  let specialCharsInputErrPara = document.createElement('p')
+  if (ev.target.value > 2) {
+    specialCharsInputErrPara = document.createElement('p')
+    specialCharsInputErrPara.textContent = 'Provide less than 3 special characters.'
+    outputDiv.append(specialCharsInputErrPara)
+  } else {
+    outputDiv.remove(specialCharsInputErrPara)
+    specialCharsLength = ev.target.value
+  }
 })
 numbersInput.addEventListener('change', ev => {
+  let numberInputErrPara = document.createElement('p')
+  if (ev.target.value > 3) {
+    numberInputErrPara = document.createElement('p')
+    numberInputErrPara.textContent = 'Provide less than 4 numbers.'
+    outputDiv.append(numberInputErrPara)
+  } else {
+    outputDiv.remove(numberInputErrPara)
+    specialCharsLength = ev.target.value
+  }
   numbersLength = ev.target.value
 })
 
