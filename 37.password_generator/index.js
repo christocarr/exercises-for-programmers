960655967790307b0d2927e8fb963ee1f8cc1d72
Chris Carr <christocarr@outlook.com>
2020-03-26 07:43:48 +0000

    Add class to input div

--- 37.password_generator/index.js
+++ 37.password_generator/index.js
@@ -1,3 +1,4 @@
+const inputDiv = document.querySelectorAll('.inputDiv')
 const lengthInput = document.getElementById('lengthInput')
 const specialCharsInput = document.getElementById('specialCharsInput')
 const numbersInput = document.getElementById('numbersInput')
@@ -8,36 +9,29 @@ let passwordLength, specialCharsLength, numbersLength
 lengthInput.addEventListener('change', ev => {
   let passwordLengthErrPara = document.createElement('p')
   if (ev.target.value < 8) {
-    passwordLengthErrPara = document.createElement('p')
     passwordLengthErrPara.textContent = 'Password must have 8 or more characters.'
-    outputDiv.append(passwordLengthErrPara)
+    inputDiv[0].append(passwordLengthErrPara)
   } else {
-    passwordLengthErrPara.remove()
     passwordLength = ev.target.value
   }
 })
 specialCharsInput.addEventListener('change', ev => {
   let specialCharsInputErrPara = document.createElement('p')
   if (ev.target.value > 2) {
-    specialCharsInputErrPara = document.createElement('p')
     specialCharsInputErrPara.textContent = 'Provide less than 3 special characters.'
-    outputDiv.append(specialCharsInputErrPara)
+    inputDiv[1].append(specialCharsInputErrPara)
   } else {
-    outputDiv.remove(specialCharsInputErrPara)
     specialCharsLength = ev.target.value
   }
 })
 numbersInput.addEventListener('change', ev => {
   let numberInputErrPara = document.createElement('p')
   if (ev.target.value > 3) {
-    numberInputErrPara = document.createElement('p')
     numberInputErrPara.textContent = 'Provide less than 4 numbers.'
-    outputDiv.append(numberInputErrPara)
+    inputDiv[2].append(numberInputErrPara)
   } else {
-    outputDiv.remove(numberInputErrPara)
-    specialCharsLength = ev.target.value
+    numbersLength = ev.target.value
   }
-  numbersLength = ev.target.value
 })
 
 const generatePassword = () => {
