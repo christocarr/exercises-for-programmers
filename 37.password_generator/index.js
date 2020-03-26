f8ab340a359baf233507e58463d89ac7a9b3fc24
Chris Carr <christocarr@outlook.com>
2020-03-26 11:40:11 +0000

    Add hard coded special chars, numbers and letters

--- 37.password_generator/index.js
+++ 37.password_generator/index.js
@@ -35,5 +35,8 @@ numbersInput.addEventListener('keyup', ev => {
 })
 
 const generatePassword = () => {
-  console.log(passwordLength, specialCharsLength, numbersLength)
+  const specialChars = ['$', '*', '£', '%', '?', '!']
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 }
