18b18640bf2a9e07082da3dd4f5ad4c0b1844be6
Chris Carr <christocarr@outlook.com>
2020-03-26 10:47:09 +0000

    Add condition to show or hide err with class

--- 37.password_generator/index.js
+++ 37.password_generator/index.js
@@ -1,4 +1,4 @@
-const inputDiv = document.querySelectorAll('.inputDiv')
+const inputErr = document.querySelectorAll('#inputErr')
 const lengthInput = document.getElementById('lengthInput')
 const specialCharsInput = document.getElementById('specialCharsInput')
 const numbersInput = document.getElementById('numbersInput')
@@ -8,37 +8,31 @@ let passwordLength, specialCharsLength, numbersLength
 
 lengthInput.addEventListener('keyup', ev => {
   passwordLength = ev.target.value
-  validateInput()
-})
-specialCharsInput.addEventListener('change', ev => {
-  let specialCharsInputErrPara = document.createElement('p')
-  if (ev.target.value > 2) {
-    specialCharsInputErrPara.textContent = 'Provide less than 3 special characters.'
-    inputDiv[1].append(specialCharsInputErrPara)
+  if (passwordLength > 7) {
+    inputErr[0].classList.add('hide')
+    inputErr[0].classList.remove('show')
   } else {
-    specialCharsLength = ev.target.value
+    inputErr[0].classList.add('show')
   }
 })
-numbersInput.addEventListener('change', ev => {
-  let numberInputErrPara = document.createElement('p')
-  if (ev.target.value > 3) {
-    numberInputErrPara.textContent = 'Provide less than 4 numbers.'
-    inputDiv[2].append(numberInputErrPara)
+specialCharsInput.addEventListener('keyup', ev => {
+  specialCharsLength = ev.target.value
+  if (specialCharsLength <= 2) {
+    inputErr[1].classList.add('hide')
+    inputErr[1].classList.remove('show')
   } else {
-    numbersLength = ev.target.value
+    inputErr[1].classList.add('show')
   }
 })
-
-const validateInput = () => {
-  const isLengthGood = passwordLength > 7 ? true : false
-  if (isLengthGood) {
-    const passwordLengthErrPara = document.createElement('p')
-    passwordLengthErrPara.textContent = 'Password must have 8 or more characters.'
-    inputDiv[0].append(passwordLengthErrPara)
+numbersInput.addEventListener('keyup', ev => {
+  numbersLength = ev.target.value
+  if (numbersLength <= 3) {
+    inputErr[2].classList.add('hide')
+    inputErr[2].classList.remove('show')
   } else {
-    
+    inputErr[2].classList.add('show')
   }
-}
+})
 
 const generatePassword = () => {
   console.log(passwordLength, specialCharsLength, numbersLength)
